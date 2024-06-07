263f27b02dc02c10e69fcdaec91ce23d447afc53
tineshsakthir <tineshsakthionline@gmail.com>
2024-06-07 19:13:21 +0530

    fix: added cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express' ; 
 import userRouter from './router/user' ; 
 import workerRouter from './router/worker' ;
+import cors from 'cors' ;
 
 const app = express() ; 
 
-app.use(express.json())
+app.use(express.json()) ; 
+
+app.use(cors()) ; 
 
 app.use('/v1/user', userRouter) ; 
 app.use('/v1/worker', workerRouter) ; 

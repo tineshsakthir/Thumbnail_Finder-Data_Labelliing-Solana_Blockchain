de43dc79a5ba307c808d3987d5aeab2ccd7f598f
tineshsakthir <tineshsakthionline@gmail.com>
2024-06-08 19:02:53 +0530

    feat: User frontend to create a task, started working on view task details(need to implement web socket to see the real time updates)

--- user-frontend/src/pages/CreateTask.tsx
+++ user-frontend/src/pages/CreateTask.tsx
@@ -1,42 +1,107 @@
 
 
-import { useState } from "react"
+import axios from "axios";
+import React, { ChangeEvent, useState } from "react"
+import { useCookies } from "react-cookie";
+import { getPresignedUrl, submitTask } from "../utils/submitTask";
 
 const CreateTask = () => {
 
+  const  [title, setTitle] = useState("") ; 
+  const [cookie, setCookie] = useCookies(["access_token"]);
+  const [imageUrl, setImageUrl] = useState<string[]>(["" , ""]);
 
-  const [imageUrl, setImageUrl] = useState([])
+  const [files, setFiles] = useState<(File | null)[]>([null, null]);
 
-  const handleChangeImage = (input: HTMLInputElement) => {
-    if (input.files && input.files[0]) {
-      const reader = new FileReader();
+  const handleChangeImage = (event: ChangeEvent<HTMLInputElement>, idx : number) => {
+    if (event.target.files && event.target.files[0]) {
 
-      reader.onload = function (e: ProgressEvent<FileReader>) {
-        const bannerImg = document.getElementById('bannerImg') as HTMLImageElement;
-        if (bannerImg) {
-          bannerImg.src = (e.target as FileReader).result as string;
-        }
-      }
 
-      reader.readAsDataURL(input.files[0]);
+      const updatedFiles = [...files] ;
+      updatedFiles[idx] = event.target.files[0] ; 
+      setFiles(updatedFiles) ; 
+      console.log(updatedFiles) ; 
+
+      const url = URL.createObjectURL(event.target.files[0]);
+      const updatedImageUrl = [...imageUrl] ; 
+      updatedImageUrl[idx] = url ; 
+      setImageUrl(updatedImageUrl) ; 
+      console.log(updatedImageUrl) ;
     }
   }
-  return (
-    <div>
-      <div className="bg-violet-500 flex gap-5  m-4 p-7 flex-col ">
-      <label htmlFor="taskName" className='text-3xl font-bold bg-pink-700'>Enter Task Name :</label>
-      <input type="text" className='bg-red-500 w-96 border border-lime-950 border-l-fuchsia-950'  name='taskName' />
-    </div>
 
-    <div className="bg-amber-700 flex gap-5  m-4 p-7 flex-col ">
-      {imageUrl.map((url : string,idx : number) => {
-        return <img key={idx} src={url} alt="" />
+  const handleAddAnotherImage = () => {
+    setImageUrl([...imageUrl, "" ]) ; 
+    setFiles([...files, null]) 
+  }
+
+  const handleDeleteChoosenImage = (delIdx : number) => {
+    if(imageUrl.length == 2) {
+      alert("Minimum two Images required to submit the task!!!")
+      return ;
+    } 
+
+    let tempImageUrl : string[] = [...imageUrl] ; 
+    tempImageUrl = tempImageUrl.filter( (_, idx) => idx!=delIdx)
+    setImageUrl(tempImageUrl) ; 
+
+    let tempFile : (File | null)[]= [...files] ;
+    tempFile = tempFile.filter((_,idx) => idx!=delIdx) ; 
+    setFiles(tempFile) ; 
+  }
+
+
+  const handleTaskSubmission = async () => {
+
+    if(title == "") {
+      alert("Please fill the title") ;
+      return ; 
+    }
+
+    for(const image of imageUrl){
+      if(image == ""){
+        alert("Please select image in all the input box or else delete them") ; 
+        return  ; 
+      }
+    }
+
+    // console.log(await getPresignedUrl(cookie.access_token)) ;
+    const res = await submitTask(cookie.access_token , files, title) ; 
+    console.log(res) ; 
+  }
+
+
+
+
+
+  return (
+    <div className="bg-pink-700 flex flex-col justify-center items-center">
+      <div className="bg-violet-500 flex gap-5  m-4 p-7 flex-col ">
+        <label htmlFor="taskName" className='text-3xl font-bold bg-pink-700'>Enter Task Name :</label>
+        <input 
+        type="text" 
+        className='bg-red-500 w-96 border border-lime-950 border-l-fuchsia-950' 
+        name='taskName'
+        onChange={(e)=> setTitle(e.target.value)} 
+        />
+      </div>
+
+      {imageUrl.map((ele: string, idx: number) => {
+        return <div className="bg-amber-700 flex gap-5  m-4 p-7 flex-col " key ={`${idx}div` }>
+          <label key ={`${idx}label` } htmlFor="taskName" className='text-3xl font-bold bg-amber-500' >Select a Photo <span className="text-red-900 bg-pink-400">**</span> </label>
+          <input key ={`${idx}photoInput` } type="file" className="bg-blue-800" onChange={(event) => { handleChangeImage(event, idx) }} required />
+          <img key ={`${idx}phto` } src={imageUrl[idx]} alt="" id={`${idx}`} />
+          <button key ={`${idx}btn` } className="bg-red-800" onClick={()=>{handleDeleteChoosenImage(idx)}}>delete Image</button>
+        </div>
       })}
-    <label htmlFor="taskName" className='text-3xl font-bold bg-pink-700' >Select a Photo : </label>
-      <input type="file" className="bg-blue-800" onChange= {(event)=>{ setImagesUrl([event.target.value]) ; console.log(event.target.value)}}/> // Wrap event.target.value in an array
-    </div>
+      <button className="bg-purple-900 w-[60%] px-4 py-2 mb-5" onClick={handleAddAnotherImage}>Add Another Image</button>
+
+      <button className="bg-green-400 w-[40%] px-4 py-2 mt-2 mb-6 rounded-2xl" onClick={handleTaskSubmission}>Sumbit</button>
     </div>
   )
 }
 
 export default CreateTask
+
+
+
---
+++ user-frontend/src/utils/submitTask.ts
@@ -0,0 +1,84 @@
+import axios from "axios";
+import { BACKEND_URL } from "../configuration/config";
+
+export const getPresignedUrl = async (access_token: string) => {
+    const response = await axios.get(`${BACKEND_URL}/user/presignedUrl` , {
+      headers : {
+        Authorization : access_token
+      }
+    }) ; 
+    return response.data
+}
+
+
+
+export const submitTask = async (access_token: string, files : (File | null)[] , title : string) => {
+
+
+    const options : {
+        option_number : number ,
+        imageUrl : string
+    }[] = [];
+    
+    
+    for(let index = 0 ; index < files.length ; index++){
+
+        const file : File | null = files[index] ; 
+        if(file == null) continue ;
+
+        const presignedUrlData = await getPresignedUrl(access_token) ; 
+        const formData = new FormData() ; 
+        // Loop through the fields object and append each field to the form data
+        const fields = presignedUrlData.fields;
+        formData.append("Content-Type" ,  fields["Content-Type"]) ;
+        formData.append("bucket", fields["bucket"]) ;
+        formData.append("X-Amz-Algorithm" , fields["X-Amz-Algorithm"]) ; 
+        formData.append("X-Amz-Credential", fields["X-Amz-Credential"]) ; 
+        formData.append("X-Amz-Date", fields["X-Amz-Date"]) ;
+        formData.append("key", fields["key"]) ;
+        formData.append("Policy", fields["Policy"]) ;
+        formData.append("X-Amz-Signature", fields["X-Amz-Signature"]) ;
+        formData.append("file", file) ; 
+
+        options.push(
+            {   
+                option_number : index+1 ,
+                imageUrl : fields["key"] 
+            }
+        )
+        const response = await axios.post(presignedUrlData.url , formData) ; 
+        console.log(response.data) ; 
+        console.log(file , "\n\nFIleE uploaded")
+    }
+
+
+    const taskInfo = {
+        options: options ,
+        signature : "myPaymentSignatureForTask 3" ,
+        title : title
+    };
+
+
+    informTaskInfoToBacked(taskInfo, access_token) ; 
+
+    return "Task successfully submitted" ; 
+}
+
+
+
+export const informTaskInfoToBacked = async (taskInfo: {
+    options : {
+        option_number : number ,
+        imageUrl : string
+    }[] , 
+    signature : string , 
+    title : string }, access_token : string) => {
+
+        await axios.post(`${BACKEND_URL}/user/task` , taskInfo , {
+            headers : {
+                Authorization : access_token
+            }
+        })
+}
+
+
